a813469c591b084b974bdd2aecf0b6de65d33255
fateme zahra <fatemezahrakalantarii@gmail.com>
2022-06-23 16:18:33 +0430

    Email and Password Reset feature added.

--- post_blog/settings.py
+++ post_blog/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
-MEDIA_URL='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -131,3 +131,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog:blog-home'
 # LOGOUT_REDIRECT_URL = 'blog:blog-home'
 LOGIN_URL = 'login'
+
+
+SERVER_EMAIL = 'fzkworks@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'fzkworks@gmail.com'
+EMAIL_HOST_PASSWORD = 'rhnsrzodwaehodnj'
